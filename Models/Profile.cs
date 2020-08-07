2a8a161cb6378ce12d0e792e01898a25d0655404
Bartek Longota <b.longota2@wp.pl>
2020-08-07 15:59:59 +0200

    Dodanie zawartosci modelu Profile

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace PapugarniaOnline.Models
 {
     public class Profile
     {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string SurName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Address { get; set; }
     }
 }
